0754e1d96f48f9f56c03d9b8b7da4a045f8d6719
Piotr Solnica <piotr.solnica+oss@gmail.com>
2018-03-31 12:05:08 +0200

    Add Test namespace with cleaning to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,3 +32,19 @@ root = Pathname(__FILE__).dirname
 
 Dir[root.join('support/**/*.rb').to_s].each { |file| require file }
 Dir[root.join('shared/**/*.rb').to_s].each { |file| require file }
+
+# Namespace holding all objects created during specs
+module Test
+  def self.remove_constants
+    constants.each(&method(:remove_const))
+  end
+end
+
+RSpec.configure do |config|
+  config.after do
+    Test.remove_constants
+  end
+
+  config.disable_monkey_patching!
+  config.warnings = true
+end
