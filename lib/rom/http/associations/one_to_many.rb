e89025e09732e065e027a8ae12528b2fc801adf5
Ian Ker-Seymer <i.kerseymer@gmail.com>
2021-02-26 14:11:43 -0500

    Clean up implementation

--- lib/rom/http/associations/one_to_many.rb
+++ lib/rom/http/associations/one_to_many.rb
@@ -5,16 +5,14 @@ require 'rom/associations/one_to_many'
 module ROM
   module HTTP
     module Associations
+      # OneToMany implementation
       class OneToMany < ROM::Associations::OneToMany
         def call(target: self.target)
+          raise MissingAssociationViewError, 'must override view' unless view
+
           schema = target.schema.qualified
           relation = target
-
-          if view
-            apply_view(schema, relation)
-          else
-            raise 'must override view'
-          end
+          apply_view(schema, relation)
         end
       end
     end
